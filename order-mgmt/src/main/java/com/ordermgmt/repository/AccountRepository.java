e7ff71c3cd551726c812d786e56cb062cc03a1d3
chpranav7 <86147288+chpranav7@users.noreply.github.com>
2022-11-13 12:15:18 +0530

    Add files via upload

---
+++ order-mgmt/src/main/java/com/ordermgmt/repository/AccountRepository.java
@@ -0,0 +1,19 @@
+package com.ordermgmt.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.ordermgmt.entity.Account;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, Long> {
+	
+	@Query(value = "SELECT DISTINCT account_number FROM account" , nativeQuery = true)
+	List<String> findDistinctAccountNumber();
+
+	List<Account> findAllByOrderByDateDesc();
+	
+}
---
+++ order-mgmt/src/main/java/com/ordermgmt/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.ordermgmt.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ordermgmt.entity.Users;
+
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+  Optional<Users> findByEmail(String email);
+
+  Boolean existsByEmail(String email);
+  
+}
