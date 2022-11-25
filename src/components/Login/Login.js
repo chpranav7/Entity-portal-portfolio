b37938f097731c1a8e62fe011d3bb8a030de33ea
chpranav7 <86147288+chpranav7@users.noreply.github.com>
2022-11-26 01:13:39 +0530

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -162,12 +162,7 @@ export default function Login({ toggleModal, loginButton }) {
                         </div>
 
 
-                        <div className="flex items-start mb-6">
-                          <div className="flex items-center h-5">
-                            <input required id="remember" type="checkbox" defaultValue className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" />
-                          </div>
-                          <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-400">I have checked the input</label>
-                        </div>
+                        
 
 
                         <span onClick={toggleForgotPassModal} class="cursor-pointer text-blue-600">Forgot password?</span>
